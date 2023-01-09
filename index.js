894f8d596c6fbdf8e9af49874ee31f33ca113f1a
Nguyen Quang THO <thonqFX17090@funix.edu.vn>
2023-01-09 23:36:57 +0700

    cors

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const userRoute = require("./routes/user");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
